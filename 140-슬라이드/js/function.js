b6aabc92822344dd4d8bc4389a1ba7f5a826f9d3
안쌤 <mvcweb@naver.com>
2024-01-15 16:47:45 +0900

    javascript

--- 140-슬라이드/js/function.js
+++ 140-슬라이드/js/function.js
@@ -1,1 +1,19 @@
-$
+const $indicators = document.querySelectorAll('#slides>.slides-pagination>li>a');
+const $container = document.querySelector('#slides>.slides-container');
+
+console.log($container);
+
+let nowIdx = 0;
+let oldIdx = nowIdx;
+
+$indicators.forEach(function($indicator, idx){
+	$indicator.addEventListener('click', function(evt){
+		evt.preventDefault();
+		
+		oldIdx = nowIdx;
+		nowIdx = idx;
+		
+		//컨테이너 이동
+		$container.style.left = -(100*nowIdx)+'%';
+	});
+});
