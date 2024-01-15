b371804b1e231a1ef5113f376cf29747aae243f3
안쌤 <mvcweb@naver.com>
2024-01-15 17:16:28 +0900

    javascript

--- 140-슬라이드/js/function.js
+++ 140-슬라이드/js/function.js
@@ -1,5 +1,7 @@
 const $indicators = document.querySelectorAll('#slides>.slides-pagination>li>a');
 const $container = document.querySelector('#slides>.slides-container');
+const $btnPrev = document.querySelector('#slides>.slides-navigation.slides-prev');
+const $btnNext = document.querySelector('#slides>.slides-navigation.slides-next');
 
 console.log($container);
 
@@ -13,7 +15,60 @@ $indicators.forEach(function($indicator, idx){
 		oldIdx = nowIdx;
 		nowIdx = idx;
 		
+		//활성화표시
+		$indicators[nowIdx].parentElement.classList.add('on');
+		$indicators[oldIdx].parentElement.classList.remove('on');
+		
 		//컨테이너 이동
 		$container.style.left = -(100*nowIdx)+'%';
 	});
 });
+
+//이전버튼
+$btnPrev.addEventListener('click', function(evt){
+	evt.preventDefault();
+	
+	oldIdx = nowIdx;
+	
+	if(nowIdx>0){
+		nowIdx--;
+	}else{
+		nowIdx=4;
+	}
+	
+	//활성화표시
+	$indicators[nowIdx].parentElement.classList.add('on');
+	$indicators[oldIdx].parentElement.classList.remove('on');
+	
+	//컨테이너 이동
+	$container.style.left = -(100*nowIdx)+'%';	
+});
+
+//다음버튼
+$btnNext.addEventListener('click', function(evt){
+	evt.preventDefault();
+	
+	oldIdx = nowIdx;
+	
+	if(nowIdx<4){
+		nowIdx++;
+	}else{
+		nowIdx=0;
+	}
+	
+	//활성화표시
+	$indicators[nowIdx].parentElement.classList.add('on');
+	$indicators[oldIdx].parentElement.classList.remove('on');
+	
+	//컨테이너 이동
+	$container.style.left = -(100*nowIdx)+'%';	
+});
+
+
+
+
+
+
+
+
+
